771d51d5013a8cace319d9433f9933d9d785f055
Mohit <mohithyper007@gmail.com>
2026-03-18 00:44:35 +0530

    connected to mongodb database in cloud server

---
+++ assign3/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+//alternative of this 
+// import dotenv from "dotenv";
+// dotenv.config();
+
+const connectDB = async()=> {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB host : ${connectionInstance.connection.host}`);
+        
+    }catch (error){
+        console.log("MongoDB ERROR",error);
+        process.exit(1);       
+    }
+}
+
+export default connectDB;
---
+++ assign3/src/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+// const app = express()
+
+
+/*
+import express from "express"
+//better approach to connect with error handling
+(async () => { //ify concept 
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/{DB_NAME}`)
+        app.on("error",(err)=>{
+            console.log("ERR: ",err);
+            throw err
+        })
+
+        app.listen(process.env.PORT,()=> {
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })}
+        catch (error){
+            console.log("Error: ",error);
+            throw error;
+            
+        }
+    }
+)() */
