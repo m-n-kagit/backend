30030bc51c12aa8fef530721dfafd75b2b7fcb2f
Mohit <mohithyper007@gmail.com>
2026-03-22 15:13:46 +0530

    assign3 updation

--- assign3/src/routes/user.routes.js
+++ assign3/src/routes/user.routes.js
@@ -3,9 +3,17 @@ import registerUser from "../controller/user.controller.js"
 import {upload} from "../middlewares/multer_basic.js"
 const router = Router()
 
-router.route("")
-
-router.post("/register",registerUser) // we are using post method for registration because
+router.route("/register").post(
+    upload.fields([ //fields taken because array takes multiple files in only one field
+        {
+            name: "avatar",
+            maxCount:1 //files accepted 
+        },{
+            name: "coverImage",
+            maxCount: 1
+        }
+    ])
+    ,registerUser) // we are using post method for registration because
 //  we are sending data to the server 
 // and also for security reasons as get method will expose the data in the url 
 // and also for caching issues
--- assign3/src/utils/ApiResponse.js
+++ assign3/src/utils/ApiResponse.js
@@ -6,3 +6,5 @@ class ApiResponse {
         this.success = statusCode >= 200 && statusCode < 300; // if status code is in the range of 200-299 then success is true otherwise false
     }
 }
+
+export default ApiResponse
